12055a876f4e8e9e6ee76504c50885c3d25c4f89
Artur Harutyunyan <arturhar@outlook.com>
2025-03-18 01:04:33 +0400

    start

---
+++ swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Artur Harutyunyan on 18.03.25.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
